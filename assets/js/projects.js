f2fbc51d3a427943891102bfeedefc332cbebad8
Edwardchen14 <58500716+Edwardchen14@users.noreply.github.com>
2019-12-03 21:41:56 -0800

    Portfolio

---
+++ assets/js/projects.js
@@ -0,0 +1,12 @@
+$('#main').on("mouseover", function () {
+    $('#overlay').show();
+});
+
+$('#overlay').on("mouseover", function () {
+    $('#overlay').show();
+});
+
+$('#main').on("mouseout", function () {
+    $('#overlay').hide();
+});
+
